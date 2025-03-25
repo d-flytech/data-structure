74114b4e7cba5749959d4c62fd666303b3bc611f
d-flytech <desirevanbrummen@hotmail.com>
2025-03-25 13:06:17 +0100

    added arrayadults3

---
+++ arrayadults3/arrayadults3.js
@@ -0,0 +1,14 @@
+
+const Person = {
+    name: ['Rob', 'Arla', 'Pam', 'Beer', 'Pippi'],
+    age: [25, 15, 21, 46, 13],
+    bsn: [123456, 257654, 315421, 463456, 533446],
+    gender: ['male', 'female', 'female', 'x', 'female'],
+};
+
+function getAdults() {
+    let adults = Person.name.filter((_, index) => Person.age[index] >= 18); 
+    console.log(`${adults.join( `, `)} are adults.`)
+};
+
+getAdults();
