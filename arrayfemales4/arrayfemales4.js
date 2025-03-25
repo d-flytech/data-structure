c7f33d48444d7ca96924ca3e98ea902b46aa5a1c
d-flytech <desirevanbrummen@hotmail.com>
2025-03-25 13:19:06 +0100

    added arrayfemales4

---
+++ arrayfemales4/arrayfemales4.js
@@ -0,0 +1,13 @@
+const Person = {
+    name: ['Rob', 'Arla', 'Pam', 'Beer', 'Pippi'],
+    age: [25, 15, 21, 46, 13],
+    bsn: [123456, 257654, 315421, 463456, 533446],
+    gender: ['male', 'female', 'female', 'x', 'female'],
+};
+
+function getFemales() {
+    let adults = Person.name.filter((_, index) => Person.gender[index] === 'female'); 
+    console.log(`${adults} are females.`)
+};
+
+getFemales();
