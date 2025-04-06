b5bb07399ce4804975e2517dd8e2cee63ffcc80b
d-flytech <desirevanbrummen@hotmail.com>
2025-04-06 21:24:31 +0200

    removed function, newPerson will be added to original array

--- arraypush6/arraypush6.js
+++ arraypush6/arraypush6.js
@@ -1,4 +1,4 @@
-const Person = [
+const personsList = [
     { name: 'Rob', age: 25, bsn: 123456, gender: 'male' },
     { name: 'Arla', age: 15, bsn: 257654, gender: 'female' },
     { name: 'Pam', age: 21, bsn: 315421, gender: 'female' },
@@ -6,14 +6,9 @@ const Person = [
     { name: 'Pippi', age: 13, bsn: 533446, gender: 'female' }
 ];
 
-function addNewPersons(newPerson) {
-     Person.push(newPerson);
-     return Person; 
-}
-
 const newPerson = {name:'Box', age: 35, bsn: 654555, gender: 'male'};
-const updatePersonList = addNewPersons(newPerson);
-console.log(updatePersonList);
+personsList.push(newPerson);
+console.log(personsList);
 
 //used Array push() to add a new person
 
