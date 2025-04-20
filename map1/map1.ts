1d67765df7510b7e4090e9b5cadebaf47ecb1a6a
d-flytech <desirevanbrummen@hotmail.com>
2025-04-20 16:20:36 +0200

    trying again adding packages

--- map1/map1.ts
+++ map1/map1.ts
@@ -1,4 +1,4 @@
 const array: number[] = [1, 2, 3, 4, 5];
 const multiplyArray: number[] = array.map((item: number) => item * 6);
 
-/* Multiply each element in the array by 6 with map(). */
+/* Multiply each element in the array by 6 with map() */
